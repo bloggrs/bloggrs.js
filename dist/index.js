582856f605b63a683e0b6e45f7a2229c35671eda
gjergjkadriu <gjergj.kadriu@bk.sli.ch>
2022-01-18 00:10:19 +0100

    push;

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
+const momentjs = document.createElement("script");
+momentjs.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js";
+document.body.appendChild(momentjs);
+Object.defineProperty(String.prototype, 'capitalize', {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false
+});
+
 function createElement(tagName, attrs = {}, ...children) {
   const elem = Object.assign(document.createElement(tagName), attrs);
 
@@ -18,7 +28,6 @@ class CategoriesListWidget extends HTMLElement {
       loading: true,
       categories: []
     };
-    this.onFirstRender = true;
   }
 
   setState = obj => {
@@ -37,19 +46,7 @@ class CategoriesListWidget extends HTMLElement {
     const root_div = document.createElement('div');
     root_div.id = "root";
     root_div.append(element);
-
-    if (!this.onFirstRender) {
-      this.innerHTML = root_div.innerHTML;
-      this.onFirstRender = false;
-      return;
-    }
-
-    const this_root = this.querySelector('#root');
-
-    if (this_root) {
-      this_root.innerHTML += root_div.innerHTML;
-      this.onFirstRender = false;
-    } else this.append(root_div);
+    this.innerHTML = root_div.innerHTML;
   };
 
   connectedCallback() {
@@ -121,9 +118,8 @@ class PostsListWidget extends HTMLElement {
     super();
     this.state = {
       loading: true,
-      posts: []
+      categories: []
     };
-    this.onFirstRender = true;
   }
 
   setState = obj => {
@@ -142,18 +138,7 @@ class PostsListWidget extends HTMLElement {
     const root_div = document.createElement('div');
     root_div.id = "root";
     root_div.append(element);
-
-    if (!this.onFirstRender) {
-      this.innerHTML = root_div.innerHTML;
-      return;
-    }
-
-    const this_root = this.querySelector('#root');
-
-    if (this_root) {
-      this_root.innerHTML += root_div.innerHTML;
-      this.onFirstRender = false;
-    } else this.append(root_div);
+    this.innerHTML = root_div.innerHTML;
   };
 
   connectedCallback() {
@@ -169,16 +154,16 @@ class PostsListWidget extends HTMLElement {
     const args = {};
     const size = this.getAttribute("size");
     if (size) args.pageSize = size;
-    const posts = await window.bloggrs.posts.getPosts(args);
+    const categories = await window.bloggrs.posts.getPosts(args);
     this.setState({
-      posts,
+      categories,
       loading: false
     });
   }
 
-  getPostsListElements = () => {
+  getCategoriesListElements = () => {
     const display_posts_count = (this.getAttribute("display_posts_count") || "false") === "true";
-    return this.state.posts.map(ctg => createElement("li", null, ctg.name, " ", display_posts_count ? `(${ctg.meta.posts_count})` : ''));
+    return this.state.categories.map(ctg => createElement("li", null, ctg.name, " ", display_posts_count ? `(${ctg.meta.posts_count})` : ''));
   };
 
   static get observedAttributes() {
@@ -203,19 +188,238 @@ class PostsListWidget extends HTMLElement {
   render() {
     const title = this.getAttribute("title");
     const {
-      loading
+      loading,
+      categories
     } = this.state;
     return createElement("div", {
-      className: "mb-14 bg-white shadow-md rounded-md justify-center"
-    }, createElement("div", {
-      className: " mx-6"
-    }, title ? createElement("h1", {
-      className: "py-3 mb-4 text-slate-700 font-medium text-xl"
-    }, title, "dasdaads") : '', createElement("ul", {
-      className: "py-4 mx-5 list-disc space-y-3"
-    }, loading ? "Loading..." : this.getPostsListElements())));
+      className: "grid grid-col-3 grid-flow-row col-span-2 gap-4"
+    }, loading ? "Loading..." : '', this.state.categories.length ? '' : 'No posts to show', this.state.categories.map(post => {
+      return createElement("div", {
+        className: "border-b-2 border-b-slate-300 col-span-3 h-full flex"
+      }, createElement("div", {
+        className: "bg-white shadow-md h-3/4 w-1/2 rounded-md"
+      }), createElement("div", {
+        className: "px-3 h-3/4 w-3/4"
+      }, createElement("h1", {
+        className: "text-slate-700 font-medium text-xl"
+      }, post.title), createElement("p", {
+        className: "py-2 text-slate-400 font-normal text-sm"
+      }, post.html_content), createElement("div", {
+        className: "flex"
+      }, createElement("p", {
+        className: "ml-24text-slate-700 font-normal text-sm"
+      }, moment(post.createdAt).format("dddd, DD MMMM, YYYY"), " \xA0\xA0\xA0 |"), createElement("p", {
+        className: "mx-4 text-slate-700 font-normal text-sm"
+      }, post.users.first_name.capitalize(), " ", post.users.last_name.capitalize())), createElement("div", {
+        className: "flex  my-2 w-full"
+      }, createElement("p", {
+        className: "w-1/2 text-blue-300 font-medium text-sm flex "
+      }, createElement("img", {
+        src: "http://localhost:3001/dist/static/icons8-heart-80.png"
+      }), createElement("span", {
+        className: "mx-2 text-center my-2"
+      }, post.meta.likes_count, " likes")), createElement("p", {
+        className: "w-1/2 right-0 text-blue-400 font-normal text-sm flex items-center justify-center"
+      }, createElement("img", {
+        src: "http://localhost:3001/dist/static/icons8-comments-80.png"
+      }), createElement("span", {
+        className: "mx-2"
+      }, post.meta.comments_count, " comments")))));
+    }));
   }
 
 }
 
 customElements.define("posts-list-widget", PostsListWidget);
+
+class HeaderWidget extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      categories: []
+    };
+  }
+
+  setState = obj => {
+    if (typeof obj !== "object") {
+      console.error("setState not called properly");
+      return;
+    }
+
+    Object.assign(this.state, obj);
+
+    this._render();
+  };
+  _render = () => {
+    const style = this.getStyleElement;
+    const element = this.render();
+    const root_div = document.createElement('div');
+    root_div.id = "root";
+    root_div.append(element);
+    this.innerHTML = root_div.innerHTML;
+  };
+
+  connectedCallback() {
+    this.fetchData();
+
+    this._render();
+  }
+
+  async fetchData() {
+    this.setState({
+      loading: true
+    });
+    const args = {};
+    const size = this.getAttribute("size");
+    if (size) args.pageSize = size;
+    const categories = await window.bloggrs.general.getBlogHeaderWidgetData(args);
+    this.setState({
+      categories,
+      loading: false
+    });
+  }
+
+  getCategoriesListElements = () => {
+    const display_posts_count = (this.getAttribute("display_posts_count") || "false") === "true";
+    return this.state.categories.map(ctg => createElement("li", null, ctg.name, " ", display_posts_count ? `(${ctg.meta.posts_count})` : ''));
+  };
+
+  static get observedAttributes() {
+    return ['title', 'display_posts_count', 'size'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom square element attributes changed.', name, oldValue, newValue);
+
+    switch (name) {
+      case "title":
+        return this._render();
+
+      case "display_posts_count":
+        return this._render();
+
+      case "size":
+        return this.fetchData();
+    }
+  }
+
+  render() {
+    const title = this.getAttribute("title");
+    const {
+      loading,
+      categories
+    } = this.state;
+    if (loading) return '';
+    return createElement("nav", {
+      className: "bg-white shadow-md max-h-96 py-5"
+    }, createElement("div", {
+      className: "mx-32 px-2 sm:px-6 lg:px-8"
+    }, createElement("div", {
+      className: "relative flex items-center justify-between h-16"
+    }, createElement("div", {
+      className: "absolute inset-y-0 left-0 flex items-center sm:hidden"
+    }, createElement("button", {
+      type: "button",
+      className: " inline-flex items-center justify-center p-2 rounded-md text-slate-400 hover:text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white ",
+      "aria-controls": "mobile-menu",
+      "aria-expanded": "false"
+    }, createElement("span", {
+      className: "sr-only"
+    }, "Open main menu"), createElement("svg", {
+      className: "block h-6 w-6",
+      xmlns: "http://www.w3.org/2000/svg",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      "aria-hidden": "true"
+    }, createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      strokeWidth: 2,
+      d: "M4 6h16M4 12h16M4 18h16"
+    })), createElement("svg", {
+      className: "hidden h-6 w-6",
+      xmlns: "http://www.w3.org/2000/svg",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      "aria-hidden": "true"
+    }, createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      strokeWidth: 2,
+      d: "M6 18L18 6M6 6l12 12"
+    })))), createElement("div", {
+      className: "flex-1 flex items-center justify-center sm:justify-start"
+    }, createElement("div", {
+      className: "w-9/12 flex-shrink-0 flex items-center"
+    }, createElement("img", {
+      className: "block w-40 lg:hidden h-8 w-auto h-auto",
+      src: "http://localhost:3001/dist/static/logo-placeholder-image.png",
+      alt: "Workflow"
+    }), createElement("img", {
+      className: "hidden absolute lg:block h-32 w-auto h-auto",
+      style: {
+        width: '12rem'
+      },
+      src: "http://localhost:3001/dist/static/logo-placeholder-image.png",
+      alt: "Workflow"
+    }), createElement("h1", {
+      className: "text-xl lg:px-52 md:px-22 font-bold text-slate-700"
+    }, this.state.categories.blog.name)), createElement("div", {
+      className: "w-3/12 hidden sm:block sm:ml-6"
+    }, createElement("div", {
+      className: "flex space-x-4"
+    }, this.state.categories.pages.map(page => createElement("a", {
+      href: page.slug,
+      className: " text-slate-600 hover:text-slate-900 px-3 py-2 rounded-md text-base font-medium "
+    }, page.name))))))), createElement("div", {
+      className: "sm:hidden",
+      id: "mobile-menu"
+    }, createElement("div", {
+      className: "px-2 pt-2 pb-3 space-y-1"
+    }, this.state.categories.pages.map(page => createElement("a", {
+      href: page.slug,
+      className: " text-slate-600 hover:text-slate-900 px-3 py-2 rounded-md text-base font-medium "
+    }, page.name)))));
+    return createElement("div", {
+      className: "grid grid-col-3 grid-flow-row col-span-2 gap-4"
+    }, loading ? "Loading..." : '', this.state.categories.length ? '' : 'No posts to show', this.state.categories.map(post => {
+      return createElement("div", {
+        className: "border-b-2 border-b-slate-300 col-span-3 h-full flex"
+      }, createElement("div", {
+        className: "bg-white shadow-md h-3/4 w-1/2 rounded-md"
+      }), createElement("div", {
+        className: "px-3 h-3/4 w-3/4"
+      }, createElement("h1", {
+        className: "text-slate-700 font-medium text-xl"
+      }, post.title), createElement("p", {
+        className: "py-2 text-slate-400 font-normal text-sm"
+      }, post.html_content), createElement("div", {
+        className: "flex"
+      }, createElement("p", {
+        className: "ml-24text-slate-700 font-normal text-sm"
+      }, moment(post.createdAt).format("dddd, DD MMMM, YYYY"), " \xA0\xA0\xA0 |"), createElement("p", {
+        className: "mx-4 text-slate-700 font-normal text-sm"
+      }, post.users.first_name.capitalize(), " ", post.users.last_name.capitalize())), createElement("div", {
+        className: "flex  my-2 w-full"
+      }, createElement("p", {
+        className: "w-1/2 text-blue-300 font-medium text-sm flex "
+      }, createElement("img", {
+        src: "http://localhost:3001/dist/static/icons8-heart-80.png"
+      }), createElement("span", {
+        className: "mx-2 text-center my-2"
+      }, post.meta.likes_count, " likes")), createElement("p", {
+        className: "w-1/2 right-0 text-blue-400 font-normal text-sm flex items-center justify-center"
+      }, createElement("img", {
+        src: "http://localhost:3001/dist/static/icons8-comments-80.png"
+      }), createElement("span", {
+        className: "mx-2"
+      }, post.meta.comments_count, " comments")))));
+    }));
+  }
+
+}
+
+customElements.define("header-widget", HeaderWidget);
--- index.jsx
+++ index.jsx
@@ -1,4 +1,14 @@
 
+const momentjs = document.createElement("script")
+momentjs.src = "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
+document.body.appendChild(momentjs)
+
+Object.defineProperty(String.prototype, 'capitalize', {
+  value: function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false
+});
 
 function createElement(tagName, attrs = {}, ...children) {
   const elem = Object.assign(document.createElement(tagName), attrs);
@@ -33,11 +43,7 @@ class CategoriesListWidget extends HTMLElement {
     const root_div = document.createElement('div')
     root_div.id = "root"
     root_div.append(element);
-    const this_root = this.querySelector('#root')
-    if (this_root) {
-      this_root.innerHTML = ''
-      this_root.append(root_div);
-    } else this.append(root_div)
+    this.innerHTML = root_div.innerHTML
   }
   connectedCallback(){
     this.fetchData();
@@ -92,6 +98,299 @@ class CategoriesListWidget extends HTMLElement {
   }
 }
 customElements.define("categories-list-widget", CategoriesListWidget);
+
+
+class PostsListWidget extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      categories: []
+    }
+  }
+  setState = obj => {
+    if (typeof(obj) !== "object") {
+      console.error("setState not called properly")
+      return;
+    }
+    Object.assign(this.state, obj);
+    this._render();
+  };
+  _render = () => {
+    const style = this.getStyleElement;
+    const element = this.render()
+    const root_div = document.createElement('div')
+    root_div.id = "root"
+    root_div.append(element);
+    this.innerHTML = root_div.innerHTML
+  }
+  connectedCallback(){
+    this.fetchData();
+    this._render()
+  }
+  async fetchData() {
+    this.setState({ loading: true })
+    const args = {};
+    const size = this.getAttribute("size");
+    if (size) args.pageSize = size;
+    const categories = await window.bloggrs.posts.getPosts(args);
+    this.setState({ categories, loading: false })
+  }
+  getCategoriesListElements = () => {
+    const display_posts_count = (this.getAttribute("display_posts_count") || "false") === "true"
+    return this.state.categories.map(ctg => (
+      <li>{ctg.name} {display_posts_count ? `(${ctg.meta.posts_count})` : '' }</li>
+    ))
+  }
+  static get observedAttributes() { return ['title', 'display_posts_count', 'size']; }
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom square element attributes changed.', name, oldValue, newValue);
+    switch (name) {
+      case "title": return this._render();
+      case "display_posts_count": return this._render()
+      case "size": return this.fetchData()
+    }
+  }
+  render() {
+    const title = this.getAttribute("title")
+    const { loading, categories } = this.state
+    return (
+      <div className="grid grid-col-3 grid-flow-row col-span-2 gap-4">
+        { loading ? "Loading..." : '' }
+        { this.state.categories.length ? '' : 'No posts to show'}
+        {
+          this.state.categories.map(post => {
+            return (
+              <div className="border-b-2 border-b-slate-300 col-span-3 h-full flex">
+                <div className="bg-white shadow-md h-3/4 w-1/2 rounded-md" />
+                <div className="px-3 h-3/4 w-3/4">
+                  <h1 className="text-slate-700 font-medium text-xl">
+                    {post.title}
+                  </h1>
+                  <p className="py-2 text-slate-400 font-normal text-sm">
+                    {post.html_content}
+                  </p>
+                  <div className="flex">
+                    <p className="ml-24text-slate-700 font-normal text-sm">
+                      {moment(post.createdAt).format("dddd, DD MMMM, YYYY")} &nbsp;&nbsp;&nbsp; |
+                      {/* Wednesday, December 22, 2021 &nbsp;&nbsp;&nbsp; | */}
+                    </p>
+                    <p className="mx-4 text-slate-700 font-normal text-sm">
+                      {post.users.first_name.capitalize()} {post.users.last_name.capitalize()}
+                    </p>
+                  </div>
+                  <div className="flex  my-2 w-full">
+                    <p className="w-1/2 text-blue-300 font-medium text-sm flex ">
+                      <img src="http://localhost:3001/dist/static/icons8-heart-80.png" />
+                      <span className="mx-2 text-center my-2">{post.meta.likes_count} likes</span>
+                    </p>
+                    <p className="w-1/2 right-0 text-blue-400 font-normal text-sm flex items-center justify-center">
+                      <img src="http://localhost:3001/dist/static/icons8-comments-80.png" />
+                      <span className="mx-2">{post.meta.comments_count} comments</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+customElements.define("posts-list-widget", PostsListWidget);
+
+class HeaderWidget extends HTMLElement {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      categories: []
+    }
+  }
+  setState = obj => {
+    if (typeof(obj) !== "object") {
+      console.error("setState not called properly")
+      return;
+    }
+    Object.assign(this.state, obj);
+    this._render();
+  };
+  _render = () => {
+    const style = this.getStyleElement;
+    const element = this.render()
+    const root_div = document.createElement('div')
+    root_div.id = "root"
+    root_div.append(element);
+    this.innerHTML = root_div.innerHTML
+  }
+  connectedCallback(){
+    this.fetchData();
+    this._render()
+  }
+  async fetchData() {
+    this.setState({ loading: true })
+    const args = {};
+    const size = this.getAttribute("size");
+    if (size) args.pageSize = size;
+    const categories = await window.bloggrs.general.getBlogHeaderWidgetData(args);
+    this.setState({ categories, loading: false })
+  }
+  getCategoriesListElements = () => {
+    const display_posts_count = (this.getAttribute("display_posts_count") || "false") === "true"
+    return this.state.categories.map(ctg => (
+      <li>{ctg.name} {display_posts_count ? `(${ctg.meta.posts_count})` : '' }</li>
+    ))
+  }
+  static get observedAttributes() { return ['title', 'display_posts_count', 'size']; }
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom square element attributes changed.', name, oldValue, newValue);
+    switch (name) {
+      case "title": return this._render();
+      case "display_posts_count": return this._render()
+      case "size": return this.fetchData()
+    }
+  }
+  render() {
+    const title = this.getAttribute("title")
+    const { loading, categories } = this.state
+    if (loading) return '';
+    return (
+      <nav className="bg-white shadow-md max-h-96 py-5">
+        <div className="mx-32 px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <button type="button" className="
+                inline-flex
+                items-center
+                justify-center
+                p-2
+                rounded-md
+                text-slate-400
+                hover:text-white hover:bg-slate-700
+                focus:outline-none
+                focus:ring-2
+                focus:ring-inset
+                focus:ring-white
+              " aria-controls="mobile-menu" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                {/*
+              Icon when menu is closed.
+
+              Heroicon name: outline/menu
+
+              Menu open: "hidden", Menu closed: "block"
+            */}
+                <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                {/*
+              Icon when menu is open.
+
+              Heroicon name: outline/x
+
+              Menu open: "block", Menu closed: "hidden"
+            */}
+                <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
+            <div className="flex-1 flex items-center justify-center sm:justify-start">
+              <div className="w-9/12 flex-shrink-0 flex items-center">
+                <img className="block w-40 lg:hidden h-8 w-auto h-auto" src="http://localhost:3001/dist/static/logo-placeholder-image.png" alt="Workflow" />
+                <img className="hidden absolute lg:block h-32 w-auto h-auto" style={{width: '12rem'}} src="http://localhost:3001/dist/static/logo-placeholder-image.png" alt="Workflow" />
+                <h1 className="text-xl lg:px-52 md:px-22 font-bold text-slate-700">{this.state.categories.blog.name}</h1>
+              </div>
+              <div className="w-3/12 hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                  {/* Current: "bg-slate-900 text-white", Default: "text-slate-300 hover:bg-slate-700 hover:text-white" */}
+                  {
+                    this.state.categories.pages.map(page => (
+                      <a href={page.slug} className="
+                        text-slate-600
+                        hover:text-slate-900
+                        px-3
+                        py-2
+                        rounded-md
+                        text-base
+                        font-medium
+                      ">{page.name}</a>
+                    ))
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Mobile menu, show/hide based on menu state. */}
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            {/* Current: "bg-slate-900 text-white", Default: "text-slate-300  hover:text-slate-900" */}
+            {
+              this.state.categories.pages.map(page => (
+                <a href={page.slug} className="
+                  text-slate-600
+                  hover:text-slate-900
+                  px-3
+                  py-2
+                  rounded-md
+                  text-base
+                  font-medium
+                ">{page.name}</a>
+              ))
+            }
+          </div>
+        </div>
+      </nav>
+    )
+    return (
+      <div className="grid grid-col-3 grid-flow-row col-span-2 gap-4">
+        { loading ? "Loading..." : '' }
+        { this.state.categories.length ? '' : 'No posts to show'}
+        {
+          this.state.categories.map(post => {
+            return (
+              <div className="border-b-2 border-b-slate-300 col-span-3 h-full flex">
+                <div className="bg-white shadow-md h-3/4 w-1/2 rounded-md" />
+                <div className="px-3 h-3/4 w-3/4">
+                  <h1 className="text-slate-700 font-medium text-xl">
+                    {post.title}
+                  </h1>
+                  <p className="py-2 text-slate-400 font-normal text-sm">
+                    {post.html_content}
+                  </p>
+                  <div className="flex">
+                    <p className="ml-24text-slate-700 font-normal text-sm">
+                      {moment(post.createdAt).format("dddd, DD MMMM, YYYY")} &nbsp;&nbsp;&nbsp; |
+                      {/* Wednesday, December 22, 2021 &nbsp;&nbsp;&nbsp; | */}
+                    </p>
+                    <p className="mx-4 text-slate-700 font-normal text-sm">
+                      {post.users.first_name.capitalize()} {post.users.last_name.capitalize()}
+                    </p>
+                  </div>
+                  <div className="flex  my-2 w-full">
+                    <p className="w-1/2 text-blue-300 font-medium text-sm flex ">
+                      <img src="http://localhost:3001/dist/static/icons8-heart-80.png" />
+                      <span className="mx-2 text-center my-2">{post.meta.likes_count} likes</span>
+                    </p>
+                    <p className="w-1/2 right-0 text-blue-400 font-normal text-sm flex items-center justify-center">
+                      <img src="http://localhost:3001/dist/static/icons8-comments-80.png" />
+                      <span className="mx-2">{post.meta.comments_count} comments</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+customElements.define("header-widget", HeaderWidget);
+
 
 
 // const categoriesListWidget = new CategoriesListWidget({ })
